a5ad6c1c1dde01f1f6cd539148b8c8de0716e6d8
asuluntas <aysunsulun@gmail.com>
2019-03-29 18:15:17 -0700

    Updated the data generator.

--- database/generate.js
+++ database/generate.js
@@ -6,10 +6,13 @@ const faker = require('faker');
 var generateReservations = function() {
 
   for (var i = 1; i <= 8; i++) {
+  // for (var i = 1; i <= 800; i++) {
 
     var randomRestaurantId = faker.random.number({"min": 1, "max": 1});
+    // var randomRestaurantId = faker.random.number({"min": 1, "max": 100});
     var randomTableNumber = faker.random.number({"min": 1, "max": 4});
     var randomDate = faker.date.between('2019-04-15', '2019-04-15');
+    // var randomDate = faker.date.between('2019-04-15', '2019-04-30');
     var randomTime = faker.random.objectElement({tenAm: 600, onePm: 780, twoPm: 840, fourPm: 960, sixPm: 1080, sevenPm: 1140, eightPm: 1200
     });
     var formattedRandomDate = ((randomDate + "").slice(0,10));
@@ -25,7 +28,8 @@ var generateReservations = function() {
 
 var generateTables = function() {
 
-  for(var restaurantId = 1; restaurantId <= 1; restaurantId++) {
+  for (var restaurantId = 1; restaurantId <= 1; restaurantId++) {
+  // for (var restaurantId = 1; restaurantId <= 100; restaurantId++) {
     var tableNumber = 1;
     for (var i = 1; i <= 4; i++) {
       var maxOccupancy = faker.random.objectElement({two: 2, four: 4, six: 6, eight: 8});
