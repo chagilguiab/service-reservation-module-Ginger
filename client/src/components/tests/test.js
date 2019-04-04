a11d1d113149e5c5c10c1015df83148ac2e30bb6
asuluntas <aysunsulun@gmail.com>
2019-04-04 00:24:51 -0700

    Add unit tests.

--- client/src/components/tests/test.js
+++ client/src/components/tests/test.js
@@ -13,6 +13,18 @@ describe('PartySize', () => {
     wrapper = shallow(<PartySize />);
     expect(wrapper.find('.partySizeSelect').length).toEqual(1);
   });
+
+  it("renders party size options when active", () => {
+    const wrapper = shallow(<PartySize/>);
+    wrapper.setState({ active: true });
+    expect(wrapper.find(".option").length).toBe(20);
+  });
+
+  it("when simulating a change on party size, select should update its value", () => {
+    const wrapper = setupMount();
+    wrapper.find('select').simulate('change',{target: { value : 4}});
+    expect(wrapper.find('select').props().value).toBe(4);
+    });
 });
 
 describe('Time', () => {
@@ -22,5 +34,17 @@ describe('Time', () => {
     wrapper = shallow(<Time />);
     expect(wrapper.find('.timeSelect').length).toEqual(1);
   });
+
+  it("renders time options when active", () => {
+    const wrapper = shallow(<Time/>);
+    wrapper.setState({ active: true });
+    expect(wrapper.find(".option").length).toBe(20);
+  });
+
+  it("when simulating a change on time, select should update its value", () => {
+    const wrapper = setupMount();
+    wrapper.find('select').simulate('change',{target: { value : 1200 }});
+    expect(wrapper.find('select').props().value).toBe(1200);
+    });
 });
 
