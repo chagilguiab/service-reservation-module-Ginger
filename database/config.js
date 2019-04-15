f680fa64597ea623fcc4ed032c0e7a9cd771e5ff
Aysun Sulun Tas <46325441+asuluntas@users.noreply.github.com>
2019-04-15 02:57:17 -0700

    update config.js

--- database/config.js
+++ database/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    //host: 'ec2-52-9-103-132.us-west-1.compute.amazonaws.com',
-  host: 'ec2-52-53-231-62.us-west-1.compute.amazonaws.com',
+  host: 'ec2-13-57-215-51.us-west-1.compute.amazonaws.com',
   port: '3306',
   user: 'root',
   password: '',
