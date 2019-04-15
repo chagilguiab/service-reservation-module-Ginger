14e402ea69113813e980b37a2c1205c72a2e54c5
Aysun Sulun Tas <46325441+asuluntas@users.noreply.github.com>
2019-04-15 03:02:21 -0700

    update config.js

--- database/config.js
+++ database/config.js
@@ -2,6 +2,6 @@ module.exports = {
   host: 'ec2-13-57-215-51.us-west-1.compute.amazonaws.com',
   port: '3306',
   user: 'root',
-  password: '',
+  password: 'muzBost&i3^meoW7bowWow',
   database: 'opentable_reservation'
 };
