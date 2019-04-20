00b6358d59e9cec4012e666e6e8b64b098d164e2
Ginger Barham <gingermostly@gmail.com>
2019-04-19 22:36:47 -0700

    Write script for generating data

--- database/seed.js
+++ database/seed.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 
 var generateReservations = function() {
   var reservations = [];
-  for (var i = 0; i < 100000; i++) {
+  for (var i = 0; i < 1000000; i++) {
     var data = {};
-    data.restaurantId = faker.random.number({"min": 1, "max": 100});
+    data.restaurantId = faker.random.number({"min": 1, "max": 100000});
     data.tableNumber = faker.random.number({"min": 1, "max": 4});
     data.date = faker.date.between('2019-05-01', '2019-12-01').toISOString().slice(0, 10);
     data.time = faker.random.objectElement({tenAm: 600, onePm: 780, twoPm: 840, fourPm: 960, sixPm: 1080, sevenPm: 1140, eightPm: 1200
@@ -17,7 +17,7 @@ var generateReservations = function() {
 
 var generateTables = function() {
   var tables = [];
-  for (var restaurantId = 1; restaurantId <= 100; restaurantId++) {
+  for (var restaurantId = 1; restaurantId <= 100000; restaurantId++) {
     var tableNumber = 1;
     for (var i = 1; i <= 4; i++) {
       var data = {};
@@ -31,17 +31,36 @@ var generateTables = function() {
   return tables;
 }
 
-fs.writeFile('./database/tables.json', JSON.stringify(generateTables()), function(err){
-  if (err) throw err;
-  console.log('YAY!')
-});
+var writeReservations = function() {
+  console.time('timing seed');
+  var count = 1;
+  while (count <= 10) {
+    fs.writeFile(`./database/reservations-${count}.json`, JSON.stringify(generateReservations()), function(err){
+      if (err) throw err;
+      console.log('YAY!')
+    });
+    count++;
+  }
+  console.timeEnd('timing seed');
+}
 
-fs.writeFile('./database/reservations.json', JSON.stringify(generateReservations()), function(err){
-  if (err) throw err;
-  console.log('YAY!')
-});
+var writeTables = function () {
+  console.time('timing seed');
+  var count = 1;
+  while (count <= 10) {
+    fs.writeFile(`./database/tables${count}.json`, JSON.stringify(generateTables()), function(err){
+      if (err) throw err;
+      console.log('YAY!')
+    });
+    count++;
+  }
+  console.timeEnd('timing seed');
+}
 
 
+
+writeReservations();
+writeTables();
 
 
 
