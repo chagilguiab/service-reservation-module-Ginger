48f50afd260fea40dc963eeab2d6babd758b02ae
Ginger Barham <gingermostly@gmail.com>
2019-04-24 11:36:56 -0700

    Install postgres and create database

--- database/index.js
+++ database/index.js
@@ -1,27 +1,43 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/open_table');
-const db = mongoose.connection;
+// mongo database setup
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/open_table');
+// const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected!')
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('connected!')
+// });
+
+// const reservationSchema = new mongoose.Schema({
+//   restaurantId: Number,
+//   tableNumber: Number,
+//   date: String,
+//   time: Number
+// });
+
+// const tableSchema = new mongoose.Schema({
+//   restaurantId: Number,
+//   tableNumber: Number,
+//   maxOccupancy: Number
+// });
+
+// const Reservation = mongoose.model('Reservation', reservationSchema);
+
+// module.exports = {
+//   Reservation, reservationSchema
+// }
+
+// postgreSQL database setup
+
+const { Client } = require('pg');
+const client = new Client({
+  database: 'open_table'
+});
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
 });
 
-const reservationSchema = new mongoose.Schema({
-  restaurantId: Number,
-  tableNumber: Number,
-  date: String,
-  time: Number
-});
-
-const tableSchema = new mongoose.Schema({
-  restaurantId: Number,
-  tableNumber: Number,
-  maxOccupancy: Number
-});
-
-const Reservation = mongoose.model('Reservation', reservationSchema);
-
-module.exports = {
-  Reservation, reservationSchema
-}
