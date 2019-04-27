fb07efda7d12b4460379b3a76539616ce75ca86e
Ginger Barham <gingermostly@gmail.com>
2019-04-26 20:15:14 -0700

    Update server file to serve up dist folder to id endpoint, remove extra code

--- server/index.js
+++ server/index.js
@@ -6,14 +6,9 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use('/:id', express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-app.get('/:id', function(req, res) {
-  const reactPath = path.join(__dirname, '/../client/dist/index.html');
-  res.sendFile(reactPath);
-});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
